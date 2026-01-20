cbf3650cadea78b43da48d165fea1de92c6d735d
J Herskowitz <jherskowitz@globallistic.com>
2026-01-19 20:09:16 -0500

    chore: rename Charts to Pop of the Tops

--- app.js
+++ app.js
@@ -6199,7 +6199,7 @@ useEffect(() => {
               React.createElement('svg', { className: 'w-4 h-4', fill: 'none', viewBox: '0 0 24 24', stroke: 'currentColor' },
                 React.createElement('path', { strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: 2, d: 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z' })
               ),
-              'Charts'
+              'Pop of the Tops'
             ),
             React.createElement('button', {
               onClick: () => navigateTo('new-releases'),
@@ -7718,7 +7718,7 @@ useEffect(() => {
                   ),
                   'Trending Now'
                 ),
-                React.createElement('h1', { className: 'text-4xl font-bold text-white mb-2' }, 'Charts'),
+                React.createElement('h1', { className: 'text-4xl font-bold text-white mb-2' }, 'Pop of the Tops'),
                 React.createElement('p', { className: 'text-white/80 text-lg' }, 'Top 50 most played albums on Apple Music')
               )
             )
